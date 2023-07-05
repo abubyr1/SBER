e3c6e4f90757a7d1f3382fa9b11d12aadba9ebb2
abubyr1 <114400285+abubyr1@users.noreply.github.com>
2023-07-05 17:56:44 +0300

    task3.py

--- task3.py
+++ task3.py
@@ -1,4 +1,4 @@
-def get_max_number(str_list):
+def get_max_number(str_list: list) -> int:
     str_list.sort(reverse=True, key=lambda x: x + x[-1] * (9 - len(x)))
     result = ''.join(str_list)
-    return result
+    return int(result)
